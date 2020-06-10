f995e86c550203fa300409c32c16bd8e0c75112d
Azirsec <43415156+Azirsec@users.noreply.github.com>
2020-06-10 19:33:30 -0400

    Added Textures to boxes
    Partially implemented pressure plates

---
+++ Karral/Assets/Scripts/Moving Objects/PressurePlate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    float startHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startHeight = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > startHeight)
+        {
+            transform.position = new Vector3(transform.position.x, startHeight, transform.position.z);
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+
+        GetComponent<Rigidbody>().AddForce(Vector3.up, ForceMode.Force);
+    }
+}
