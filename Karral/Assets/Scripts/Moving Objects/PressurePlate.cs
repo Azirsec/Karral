028d52575b8a0d17b3373045a851e5f1b408fc76
Azirsec <43415156+Azirsec@users.noreply.github.com>
2020-06-12 17:39:14 -0400

    Finished pressure plates
    nearly completed another level, involving using pressure plates to open gates to get to a key

--- Karral/Assets/Scripts/Moving Objects/PressurePlate.cs
+++ Karral/Assets/Scripts/Moving Objects/PressurePlate.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
+    [SerializeField] GameObject plate;
+    [SerializeField] GameObject linkedObject;
+    [SerializeField] bool showObjectWhenPressed;
+
     float startHeight;
 
     // Start is called before the first frame update
     void Start()
     {
-        startHeight = transform.position.y;
+        startHeight = plate.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y > startHeight)
+        if (plate.transform.position.y > startHeight)
         {
-            transform.position = new Vector3(transform.position.x, startHeight, transform.position.z);
-            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            plate.transform.position = new Vector3(plate.transform.position.x, startHeight, plate.transform.position.z);
+            plate.GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
 
-        GetComponent<Rigidbody>().AddForce(Vector3.up, ForceMode.Force);
+        plate.GetComponent<Rigidbody>().AddForce(Vector3.up, ForceMode.Force);
+
+        if (plate.transform.position.y < startHeight - 0.05f)
+        {
+            linkedObject.SetActive(showObjectWhenPressed);
+        }
+        else
+        {
+            linkedObject.SetActive(!showObjectWhenPressed);
+        }
     }
 }
