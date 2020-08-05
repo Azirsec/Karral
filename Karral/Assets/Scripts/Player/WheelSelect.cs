e04ab8a5e1b26c123f8616dd02a9a3a85958fd0c
Azirsec <43415156+Azirsec@users.noreply.github.com>
2020-08-05 19:49:13 -0400

    added covers to animal select
    when animals are locked, they have a greyed out background on the select wheel

--- Karral/Assets/Scripts/Player/WheelSelect.cs
+++ Karral/Assets/Scripts/Player/WheelSelect.cs
@@ -14,21 +14,33 @@ public class WheelSelect : MonoBehaviour
     [SerializeField] PlayerRhino rhino;
     [SerializeField] PlayerMouse mouse;
 
+    [SerializeField] GameObject gorillaCover;
+    [SerializeField] GameObject mouseCover;
+    [SerializeField] GameObject rhinoCover;
+    [SerializeField] GameObject eagleCover;
+
     // Update is called once per frame
     void Update()
     {
+        // show wheel select
         if (Input.GetMouseButtonDown(1))
         {
             image.enabled = true;
             GetComponent<RectTransform>().position = Input.mousePosition;
+            
+            updateCovers();
+            
             Time.timeScale = 0.2f;
             Time.fixedDeltaTime = Time.timeScale * 0.02f;
         }
-
+        // select animal and hide wheel select
         if (Input.GetMouseButtonUp(1))
         {
             image.enabled = false;
+            
             chooseAnimal();
+            hideCovers();
+
             Time.timeScale = 1;
             Time.fixedDeltaTime = Time.timeScale * 0.02f;
         }
@@ -62,26 +74,26 @@ public class WheelSelect : MonoBehaviour
         }
         else if (mouseAng > 108)
         {
+            if (HubStorage.mouseDNA)
+            {
+                DeactivateAll();
+                mouse.Activate();
+            }
+        }
+        else if (mouseAng < -36 && mouseAng >= -108)
+        {
             if (HubStorage.eagleDNA)
             {
                 DeactivateAll();
                 eagle.Activate();
             }
         }
-        else if (mouseAng < -36 && mouseAng >= -108)
+        else if (mouseAng < -108)
         {
             if (HubStorage.rhinoDNA)
             {
                 DeactivateAll();
                 rhino.Activate();
-            }
-        }
-        else if (mouseAng < -108)
-        {
-            if (HubStorage.mouseDNA)
-            {
-                DeactivateAll();
-                mouse.Activate();
             }
         }
     }
@@ -93,5 +105,21 @@ public class WheelSelect : MonoBehaviour
         eagle.Deactivate();
         rhino.Deactivate();
         mouse.Deactivate();
+    }
+
+    void updateCovers()
+    {
+        gorillaCover.SetActive(!HubStorage.gorillaDNA);
+        mouseCover.SetActive(!HubStorage.mouseDNA);
+        rhinoCover.SetActive(!HubStorage.rhinoDNA);
+        eagleCover.SetActive(!HubStorage.eagleDNA);
+    }
+
+    void hideCovers()
+    {
+        gorillaCover.SetActive(false);
+        mouseCover.SetActive(false);
+        rhinoCover.SetActive(false);
+        eagleCover.SetActive(false);
     }
 }
