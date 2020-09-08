fe31ed5ee53dc808030317a9a8df417a91f70a26
Azirsec <43415156+Azirsec@users.noreply.github.com>
2020-09-08 14:37:48 -0400

    Added particles to Rhino walls

--- Karral/Assets/Scripts/Interactables/DestroyableWall.cs
+++ Karral/Assets/Scripts/Interactables/DestroyableWall.cs
@@ -6,12 +6,36 @@ public class DestroyableWall : MonoBehaviour
 {
     [SerializeField] float destructionForce;
 
+    [SerializeField] bool floor;
+
+    [SerializeField] ParticleSystem explodeRight;
+    [SerializeField] ParticleSystem explodeLeft;
+    [SerializeField] ParticleSystem explodeDown;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.impulse.magnitude > destructionForce)
         {
             collision.gameObject.GetComponent<Rigidbody>().AddForce(-collision.impulse * 0.75f, ForceMode.Impulse);
-            Destroy(gameObject);
+
+            if (!floor)
+            {
+                if (collision.impulse.x < 0)
+                {
+                    explodeRight.Play();
+                }
+                else
+                {
+                    explodeLeft.Play();
+                }
+            }
+            else
+            {
+                explodeDown.Play();
+            }
+
+            GetComponentInChildren<MeshRenderer>().enabled = false;
+            GetComponent<BoxCollider>().enabled = false;
         }
     }
 }   
